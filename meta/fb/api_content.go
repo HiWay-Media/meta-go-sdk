da8b12f9201d481fdeb8b65e5135552383af51d0
Allan Nava <allannava95@gmail.com>
2024-09-30 14:27:49 +0200

    solve build

--- meta/fb/api_content.go
+++ meta/fb/api_content.go
@@ -23,12 +23,11 @@ In caso di azione eseguita correttamente, l'app riceve una risposta JSON con l'I
 	}
 */
 func (s *fbService) UploadMedia(fileUrl, pageId, title, description string) (*UploadMediaResponse, error) {
-	params := &mediaBodyRequest{
-        AccessToken:        s.accessToken,
-        Title:              title,
-        Description:        description,
-        FileUrl:            fileUrl,
-    }
+	params := make(map[string]string)
+	params["access_token"] = s.accessToken
+	params["title"] = title
+	params["description"] = description
+	params["file_url"] = fileUrl
 	//
 	resp, err := s.restyPostWithQueryParams(apiUploadMediaUrl(pageId), nil, params)
 	if err != nil {
